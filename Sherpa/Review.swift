a9ab867d39b744fb9bf2f82af003e3913ea7d697
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-08-08 15:25:22 -0700

    Added code for placeholder text in text view and checked for sending review without entering content

--- Sherpa/Review.swift
+++ Sherpa/Review.swift
@@ -2,25 +2,40 @@
 //  Review.swift
 //  Sherpa
 //
-//  Created by Derek Argueta on 7/18/15.
-//  Copyright (c) 2015 Derek Argueta. All rights reserved.
+//  Created by Arun Rawlani on 7/18/15.
+//  Copyright (c) 2015 Arun Rawlani. All rights reserved.
 //
 
-import UIKit
 
-class Review: NSObject {
-  
-  var text: String
-  var userId: String
-  var rating: Double
-  
-  init(text: String, userId: String, rating: Double) {
+import Foundation
+import Parse
+
+class Review:PFObject, PFSubclassing{
     
-    self.text = text
-    self.userId = userId
-    self.rating = rating
     
-    super.init()
-  }
-   
+    @NSManaged var fromUser: PFUser?
+    @NSManaged var toTour: Tour?
+    @NSManaged var ratingGiven: Int
+    @NSManaged var reviewContent: String?
+    
+    
+    //MARK : PFSubclassing Protocol
+    
+    static func parseClassName() -> String {
+        return "Review"
+    }
+    
+    override init(){
+        super.init()
+    }
+    
+    override class func initialize() {
+        var onceToken : dispatch_once_t = 0;
+        dispatch_once(&onceToken) {
+            // inform Parse about this subclass
+            self.registerSubclass()
+        }
+    }
+    
 }
+
--- Sherpa/ReviewViewController.swift
+++ Sherpa/ReviewViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import Parse
 
-class ReviewViewController: UIViewController, FloatRatingViewDelegate {
+class ReviewViewController: UIViewController, FloatRatingViewDelegate, UITextViewDelegate {
     
-    @IBOutlet var ratingSegmentedControl: UISegmentedControl!
     @IBOutlet var floatRatingView: FloatRatingView!
-    @IBOutlet var liveLabel: UILabel!
-    @IBOutlet var updatedLabel: UILabel!
+    @IBOutlet weak var reviewContent: UITextView!
+    @IBOutlet weak var postButton: UIButton!
+    var enteredText: Bool = false
+    
+    let PLACEHOLDER_TEXT = "Example: The tour has been life defining. There was expectional hardwork put into it and I would torally recommend this to a friend. Awesome job!"
+    
+    var finalRating: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,17 +34,22 @@ class ReviewViewController: UIViewController, FloatRatingViewDelegate {
         self.floatRatingView.contentMode = UIViewContentMode.ScaleAspectFit
         self.floatRatingView.maxRating = 5
         self.floatRatingView.minRating = 1
-        self.floatRatingView.rating = 2.5
+        self.floatRatingView.rating = 3
+        self.finalRating = Int(self.floatRatingView.rating)
         self.floatRatingView.editable = true
-        self.floatRatingView.halfRatings = true
+        self.floatRatingView.halfRatings = false
         self.floatRatingView.floatRatings = false
         
-        // Segmented control init
-        self.ratingSegmentedControl.selectedSegmentIndex = 1
         
         // Labels init
-        self.liveLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
-        self.updatedLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        var liveText = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        println("Live:\(liveText)")
+        var updatedText = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        println("Updated load:\(updatedText)")
+        
+        applyPlaceholderStyle(reviewContent!, placeholderText: PLACEHOLDER_TEXT)
+        
+        reviewContent.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -47,6 +57,27 @@ class ReviewViewController: UIViewController, FloatRatingViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func reviewSubmitted(sender: AnyObject) {
+        if (!self.enteredText){
+            //wrong
+            println("Enter something")
+        }
+        else{
+        var newReview = Review()
+        newReview.fromUser = PFUser.currentUser()!
+        //get the tour thing from last class
+        newReview.ratingGiven = self.finalRating
+        newReview.reviewContent = self.reviewContent.text
+        newReview.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                println("The object has been saved.")
+            } else {
+                println("There was a problem, check error.description")
+            }
+        }
+      }
+    }
     @IBAction func ratingTypeChanged(sender: UISegmentedControl) {
         self.floatRatingView.halfRatings = sender.selectedSegmentIndex==1
         self.floatRatingView.floatRatings = sender.selectedSegmentIndex==2
@@ -55,11 +86,84 @@ class ReviewViewController: UIViewController, FloatRatingViewDelegate {
     // MARK: FloatRatingViewDelegate
     
     func floatRatingView(ratingView: FloatRatingView, isUpdating rating:Float) {
-        self.liveLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        var liveText = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        println("Live:\(liveText)")
     }
     
     func floatRatingView(ratingView: FloatRatingView, didUpdate rating: Float) {
-        self.updatedLabel.text = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        var updatedText = NSString(format: "%.2f", self.floatRatingView.rating) as String
+        println("Updated inside:\(updatedText)")
+        self.finalRating = Int(floatRatingView.rating)
+    }
+    
+    // MARK: helper functions
+    
+    func applyPlaceholderStyle(aTextview: UITextView, placeholderText: String)
+    {
+        // make it look (initially) like a placeholder
+        aTextview.textColor = UIColor.lightGrayColor()
+        aTextview.font = UIFont(name: "AvenirNext-Regular", size: 14)
+        aTextview.text = placeholderText
+    }
+    
+    func applyNonPlaceholderStyle(aTextview: UITextView)
+    {
+        // make it look like normal text instead of a placeholder
+        aTextview.textColor = UIColor.whiteColor()
+        aTextview.font = UIFont(name: "AvenirNext-Regular", size: 14)
+        aTextview.alpha = 1.0
+    }
+    
+    func moveCursorToStart(aTextView: UITextView)
+    {
+        dispatch_async(dispatch_get_main_queue(), {
+            aTextView.selectedRange = NSMakeRange(0, 0);
+        })
+    }
+    
+    // MARK: UITextViewDelegate
+    
+    func textViewShouldBeginEditing(aTextView: UITextView) -> Bool
+    {
+        if aTextView == reviewContent && aTextView.text == PLACEHOLDER_TEXT
+        {
+            // move cursor to start
+            moveCursorToStart(aTextView)
+        }
+        return true
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        
+        // remove the placeholder text when they start typing
+        // first, see if the field is empty
+        // if it's not empty, then the text should be black and not italic
+        // BUT, we also need to remove the placeholder text if that's the only text
+        // if it is empty, then the text should be the placeholder
+        let newLength = count("textView.text".utf16) + count(text.utf16) - range.length
+        self.enteredText = true
+        if newLength > 0 // have text, so don't show the placeholder
+        {
+            // check if the only text is the placeholder and remove it if needed
+            // unless they've hit the delete button with the placeholder displayed
+            if textView == reviewContent && textView.text == PLACEHOLDER_TEXT
+            {
+                if count(text.utf16) == 0 // they hit the back button
+                {
+                    return false // ignore it
+                }
+                applyNonPlaceholderStyle(textView)
+                textView.text = ""
+            }
+            return true
+        }
+        else  // no text, so show the placeholder
+        {
+            applyPlaceholderStyle(textView, placeholderText: PLACEHOLDER_TEXT)
+            moveCursorToStart(textView)
+            self.enteredText = false
+            return false
+        }
     }
     
     
