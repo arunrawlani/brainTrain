d16c5cf8f499c052a4fd7f0a722c0fa56c24f61e
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-09-12 22:36:36 -0400

    Added graphs and view controller

---
+++ Sherpa/GraphEEGView.swift
@@ -0,0 +1,163 @@
+//
+//  GraphEEGView.swift
+//  MHacksViews
+//
+//  Created by Shreyan Jain on 9/12/15.
+//  Copyright (c) 2015 Shreyan Jain. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class GraphEEGView: UIView {
+    var plotValues: [Double]!
+    var xLabels: [String]!
+    var count: Int!
+    
+    let startColor: UIColor = UIColor(red: 255.0/255.0, green: 188.0/255.0, blue:143.0/255.0, alpha: 1.0);
+    let endColor: UIColor = UIColor(red: 255.0/255.0, green: 63.0/255.0, blue:21.0/255.0, alpha: 1.0);
+    
+    init(values:[Double], labels:[String], count:Int, frame:CGRect) {
+        plotValues = values;
+        xLabels = labels
+        self.count = count
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func drawRect(rect: CGRect) {
+        
+        let width = rect.width
+        let height = rect.height
+        
+        //2 - get the current context
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [startColor.CGColor, endColor.CGColor]
+        
+        //3 - set up the color space
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        //4 - set up the color stops
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        
+        //5 - create the gradient
+        let gradient = CGGradientCreateWithColors(colorSpace,
+            colors,
+            colorLocations)
+        
+        //6 - draw the gradient
+        var startPoint = CGPoint.zeroPoint
+        var endPoint = CGPoint(x:0, y:self.bounds.height)
+        CGContextDrawLinearGradient(context,
+            gradient,
+            startPoint,
+            endPoint,
+            0)
+        
+        let margin:CGFloat = 20;
+        var columnXPoint = { (column:Int) -> CGFloat in
+            //Calculate gap between points
+            let spacer = (width - margin*2 - 4) /
+                CGFloat((self.count - 1))
+            var x:CGFloat = CGFloat(column) * spacer
+            x += margin + 2
+            return x
+        }
+        
+        let topBorder:CGFloat = 20
+        let bottomBorder:CGFloat = 10
+        let graphHeight = height - topBorder - bottomBorder
+        let maxValue = maxElement(plotValues)
+        let minValue = minElement(plotValues)
+        var columnYPoint = { (graphPoint:Double) -> CGFloat in
+            var y:CGFloat = (CGFloat(graphPoint) - CGFloat(minValue)) /
+                (CGFloat(maxValue) - CGFloat(minValue)) * graphHeight
+            y = graphHeight + topBorder - y // Flip the graph
+            return y
+        }
+        
+        // draw the line graph
+        
+        UIColor.whiteColor().setFill()
+        UIColor.whiteColor().setStroke()
+        
+        //set up the points line
+        var graphPath = UIBezierPath()
+        //go to start of line
+        graphPath.moveToPoint(CGPointMake(columnXPoint(0),
+            columnYPoint(plotValues[0])))
+        
+        //add points for each item in the graphPoints array
+        //at the correct (x, y) for the point
+        for i in 1..<plotValues.count {
+            let nextPoint = CGPointMake(columnXPoint(i),
+                columnYPoint(plotValues[i]))
+            graphPath.addLineToPoint(nextPoint)
+        }
+        
+        //1 - save the state of the context 
+        CGContextSaveGState(context)
+        
+        //2 - make a copy of the path
+        var clippingPath = graphPath.copy() as! UIBezierPath
+        
+        //3 - add lines to the copied path to complete the clip area
+        clippingPath.addLineToPoint(CGPoint(
+        x: columnXPoint(plotValues.count - 1),
+        y:height))
+        clippingPath.addLineToPoint(CGPoint(
+        x:columnXPoint(0),
+        y:height))
+        clippingPath.closePath()
+        
+        //4 - add the clipping path to the context
+        clippingPath.addClip()
+        
+        let highestYPoint = columnYPoint(maxValue)
+        startPoint = CGPoint(x:margin, y: highestYPoint)
+        endPoint = CGPoint(x:margin, y:self.bounds.height)
+        
+        CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0)
+        CGContextRestoreGState(context)
+        
+        graphPath.lineWidth = 2.0
+        graphPath.stroke()
+        
+        for i in 0..<plotValues.count {
+            var point = CGPoint(x:columnXPoint(i), y:columnYPoint(plotValues[i]))
+            point.x -= 5.0/2
+            point.y -= 5.0/2
+            
+            let circle = UIBezierPath(ovalInRect:
+                CGRect(origin: point,
+                    size: CGSize(width: 5.0, height: 5.0)))
+            circle.fill()
+        }
+        
+        var linePath = UIBezierPath()
+        
+        //top line
+        linePath.moveToPoint(CGPoint(x:margin, y: topBorder))
+        linePath.addLineToPoint(CGPoint(x: width - margin,
+            y:topBorder))
+        
+        //center line
+        linePath.moveToPoint(CGPoint(x:margin,
+            y: graphHeight/2 + topBorder))
+        linePath.addLineToPoint(CGPoint(x:width - margin,
+            y:graphHeight/2 + topBorder))
+        
+        //bottom line
+        linePath.moveToPoint(CGPoint(x:margin,
+            y:height - bottomBorder))
+        linePath.addLineToPoint(CGPoint(x:width - margin,
+            y:height - bottomBorder))
+        let color = UIColor(white: 1.0, alpha: 0.3)
+        color.setStroke()
+        
+        linePath.lineWidth = 1.0
+        linePath.stroke()
+    }
+}
---
+++ Sherpa/ResultsViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ResultsViewController.swift
+//  autism
+//
+//  Created by Praynaa Rawlani on 9/12/15.
+//  Copyright (c) 2015 Arun Rawlani. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    var alphaGraph : GraphEEGView!
+    var betaGraph : GraphEEGView!
+    var thetaGraph : GraphEEGView!
+    var wavesTitle : UILabel!
+    var timeTitle : UILabel!
+    var graphOpen : Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var alphaVals = [3.0, 5.0, 4.0, 7.0, 3.0];
+        var betaVals = [1.0, 5.0, 7.0, 2.0, 9.0];
+        var thetaVals = [5.0, 5.0, 8.0, 5.0, 6.0];
+        
+        alphaGraph = GraphEEGView(values: alphaVals, labels: [""], count: alphaVals.count, frame: CGRectMake(20, 50, 335, 200))
+        betaGraph = GraphEEGView(values: betaVals, labels: [""], count: betaVals.count, frame: CGRectMake(20, 50, 335, 200))
+        thetaGraph = GraphEEGView(values: thetaVals, labels: [""], count: thetaVals.count, frame: CGRectMake(20, 50, 335, 200))
+        self.view.addSubview(alphaGraph)
+        graphOpen = 0
+        
+        wavesTitle = UILabel(frame:CGRectMake(0, 0, 120, 20))
+        wavesTitle.center = CGPointMake(alphaGraph.center.x, alphaGraph.center.y - alphaGraph.frame.height/2 - 15);
+        wavesTitle.textAlignment = NSTextAlignment.Center
+        wavesTitle.text = "Alpha Waves"
+        self.view.addSubview(wavesTitle)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target:self, action:"toLeft")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        swipeLeft.numberOfTouchesRequired = 2
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        var swipeRight = UISwipeGestureRecognizer(target:self, action:"toRight")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        swipeRight.numberOfTouchesRequired = 2
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var timeVals = [3.0, 3.2, 3.3, 2.9, 3.6];
+        let timeGraph = GraphReactTimeView(values: timeVals, labels: [""], count: timeVals.count, frame: CGRectMake(20, 300, 335, 200))
+        self.view.addSubview(timeGraph);
+        timeTitle = UILabel(frame:CGRectMake(0, 0, 120, 20))
+        timeTitle.center = CGPointMake(timeGraph.center.x, timeGraph.center.y - timeGraph.frame.height/2 - 15);
+        timeTitle.textAlignment = NSTextAlignment.Center
+        timeTitle.text = "Reaction Times"
+        self.view.addSubview(timeTitle)
+    }
+    
+    func toRight() {
+        if let x = graphOpen {
+            if (x == 1) {
+                betaGraph.removeFromSuperview()
+                self.view.addSubview(alphaGraph)
+                wavesTitle.text = "Alpha"
+                graphOpen = 0;
+            } else if (x == 2) {
+                thetaGraph.removeFromSuperview()
+                self.view.addSubview(betaGraph)
+                wavesTitle.text = "Beta"
+                graphOpen = 1;
+            }
+        }
+    }
+    
+    func toLeft() {
+        if let x = graphOpen {
+            if (x == 1) {
+                betaGraph.removeFromSuperview()
+                self.view.addSubview(thetaGraph)
+                wavesTitle.text = "Theta"
+                graphOpen = 2;
+            } else if (x == 0) {
+                alphaGraph.removeFromSuperview()
+                self.view.addSubview(betaGraph)
+                wavesTitle.text = "Beta"
+                graphOpen = 1;
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
